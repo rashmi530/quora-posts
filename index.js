8ec4b401d552995ec7397360c595d2ba876fa2dc
Rashmi Deep <rashmideep090@gmail.com>
2025-10-29 11:22:22 +0530

    My first project upload

---
+++ index.js
@@ -0,0 +1,71 @@
+ const express=require("express");
+ const app=express();
+ const port=8080;
+ const path=require("path");
+const {v4:uuidv4} = require('uuid');
+uuidv4();
+const methodOverride=require("method-override");
+ app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+ app.set("view wngine","ejs");
+ app.set("views",path.join(__dirname,"views"));
+ app.use(express.static(path.join(__dirname,"public")));
+
+ let posts=[
+    {
+      id:uuidv4(),
+    username:"rashmi",
+    content:"i love coding"
+ },
+ {
+   id:uuidv4(),
+    username:"akshit ",
+    content:"i love making money"
+ },
+ {
+   id:uuidv4(),
+    username:"riya",
+    content:"i love to dance"
+ },
+ ];
+ app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+ });
+ app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+ });
+ app.post("/posts",(req,res)=>{
+ let {username,content}=req.body;
+ let id=uuidv4();
+ posts.push({id,username,content});
+    res.redirect("/posts");
+ });
+ app.get("/posts/:id",(req,res)=>{
+ let{id}=req.params;
+let post=posts.find((p)=>id===p.id);
+console.log(post);
+ res.render("show.ejs",{post});
+ });
+
+ app.patch("/posts/:id",(req,res)=>{
+   let{id}=req.params;
+   let newContent=req.body.content;
+let post=posts.find((p)=>id===p.id);
+post.content=newContent;
+console.log(post);
+   res.redirect("/posts");
+ });
+ app.get("/posts/:id/edit",(req,res)=>{
+   let{id}=req.params;
+   let post=posts.find((p)=>id===p.id);
+   res.render("edit.ejs",{post});
+ });
+ app.delete("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+   posts =posts.filter((p)=>id!==p.id);
+   res.redirect("/posts");
+
+ })
+ app.listen(port,()=>{
+    console.log("listening on port 8080");
+ });
